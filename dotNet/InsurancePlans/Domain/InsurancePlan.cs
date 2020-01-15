c2859ee6c8e3f0a9a57418239fa8563e93d7a144
stevechanlee <stevechanlee@gmail.com>
2020-01-15 10:49:54 -0800

    Add files via upload

---
+++ dotNet/InsurancePlans/Domain/InsurancePlan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sabio.Models.Domain.InsurancePlans
+{
+    public class InsurancePlan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public InsuranceProviderBase InsuranceProvider { get; set; }
+        public PlanLevel PlanLevel { get; set; }
+        public PlanType PlanType { get; set; }
+        public PlanStatus PlanStatus { get; set; }
+    }
+}
---
+++ dotNet/InsurancePlans/Requests/InsurancePlanUpdateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sabio.Models.Requests.InsurancePlans
+{
+    public class InsurancePlanUpdateRequest : InsurancePlanAddRequest, IModelIdentifier
+    {
+        [Required, Range(1, Int32.MaxValue)]
+        public int Id { get; set; }
+    }
+}
